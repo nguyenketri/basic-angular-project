b6a8b0a08dc81b22677b449ec80742cb5ac5f2e8
nguyenketri <nguyenketri1803@gmail.com>
2025-09-04 21:27:21 +0700

    ngIf and ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,19 @@ import { HeaderLayoutComponent } from './shared/header-layout/header-layout.comp
 import { FormsModule } from '@angular/forms';
 import { CurrencyPipe } from './shared/pipes/CurrencyPipe.pipe';
 import { UpperCasePipe } from './shared/pipes/UpperCasePipe.pipe';
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderLayoutComponent,FormsModule,CurrencyPipe,UpperCasePipe,NgFor],
+  imports: [RouterOutlet,
+    HeaderLayoutComponent,
+    FormsModule,
+    CurrencyPipe,
+    UpperCasePipe,
+    NgFor,
+    NgClass,
+    NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -57,4 +64,14 @@ export class AppComponent {
     {name:'Shoe 3',price:500000, image:'assets/images/shoe.jpg'},
     {name:'Shoe 4',price:700000, image:'assets/images/shoe.jpg'},
    ]
+
+   // Directive gôm có 3 cái : Component, Structural, Attribute
+   // Component : Template HTML và UI - Component
+   // Structural: nglf, ngFor, ngSwitch cho phép thay đổi cấu trúc DOM thêm or delete
+   // Attribute: ngClass, ngStyle, hidden, disable,.. Cho phép thay đổi hành vi của phần tử
+    isActive = true
+    handleActive(): void{
+      this.isActive = !this.isActive
+    }
+    isVisible = false
 }
