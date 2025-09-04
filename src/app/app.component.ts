ad3f9c94780d1715890e57f0959d1ea8d10b347a
nguyenketri <nguyenketri1803@gmail.com>
2025-09-04 10:02:51 +0700

    run project and import and export component in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLayoutComponent } from './shared/header-layout/header-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,HeaderLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-basic-project';
+  
 }
